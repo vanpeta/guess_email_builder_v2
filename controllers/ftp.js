f3d42109c01e3335a53d2dbbea80524a2b852ecc
Carlos Allende <callende@Carlos-Allendes-mac-mini.local>
2017-02-27 15:00:19 -0800

    calendar and brands added

--- controllers/ftp.js
+++ controllers/ftp.js
@@ -16,19 +16,22 @@ var ftpConfig = {
 }
 
 function getInfo (req, res, next) {
+	var brand = req.query.brand;
 	var c = new Client();
 	//next line needs to be removed for security and find out how to do it without it. You will the next error { [Error: unable to verify the first certificate] code: 'UNABLE_TO_VERIFY_LEAF_SIGNATURE' } 
 	process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 	c.connect(ftpConfig);
 	c.on('ready', function () {
-		console.log('ready');
-		c.list(function (err, list) {
+		c.cwd('/43877/GuessUS/'+brand+'/Emails', function (err, currentDir) {
 			if (err) throw err;
-			console.log(list);
-			c.end();
-		})
-	})
-	res.json({res: "working"})
+			c.list(function (err, list) {
+				if (err) throw err;
+				console.log(list);
+				res.json({res: list})
+				c.end();
+			});
+		});
+	});
 }
 
 function postImage (req, res, next) {
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,20 @@
-console.log('loaded');
+var checkboxes = $('.brands');
+var calendar = $("#datepicker");
+calendar.datepicker();
 
 function getInfo () {
-	console.log('click')
+	var datePicked = calendar.datepicker('getDate');
+	var year = datePicked.getFullYear();
+	var month = datePicked.toLocaleString('en-us', {month: 'long'});
+	var day = datePicked.getDate();
+	var checked;
+	for (var i = 0; i < checkboxes.length; i++) {
+		if (checkboxes[i].checked) {
+			checked = checkboxes[i].value;
+		}
+	}
 	$.ajax({
-		url: '/connect',
+		url: '/connect?brand='+checked,
 		method: 'GET',
 		success: function (res) {
 			console.log(res);
