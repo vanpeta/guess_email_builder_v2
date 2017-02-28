4299803cb0d3ced96744d9ef49c08c0f056f9635
Carlos Allende <callende@Carlos-Allendes-mac-mini.local>
2017-02-27 17:24:15 -0800

    mkdir -r working

--- controllers/ftp.js
+++ controllers/ftp.js
@@ -17,13 +17,42 @@ var ftpConfig = {
 
 function getInfo (req, res, next) {
 	var brand = req.query.brand;
+	console.log('brand=', brand);
+	var year = req.query.year;
+	console.log('year=', year);
+	var month = req.query.month;
+	console.log('month=', month);
+	var day = req.query.day;
+	console.log('day=', day);
 	var c = new Client();
 	//next line needs to be removed for security and find out how to do it without it. You will the next error { [Error: unable to verify the first certificate] code: 'UNABLE_TO_VERIFY_LEAF_SIGNATURE' } 
 	process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 	c.connect(ftpConfig);
 	c.on('ready', function () {
-		c.cwd('/43877/GuessUS/'+brand+'/Emails', function (err, currentDir) {
-			if (err) throw err;
+		c.cwd('/43877/GuessUS/'+brand+'/Emails/'+year+'/'+'month'+'/'+'day', function (err, currentDir) {
+			if (err) {
+				if (err.code == 550) {
+					c.mkdir('/43877/GuessUS/'+brand+'/Emails/'+year+'/'+month+'/'+day, true, function (err) {
+						if (err) {
+							console.log('error creando');
+							throw err;
+						}
+						console.log('mkdir no err');
+						c.cwd('/43877/GuessUS/'+brand+'/Emails/'+year+'/'+month+'/'+day, function (err, currentDir) {
+							if (err) {
+								console.log('error entrando');
+								throw err;
+							}
+							c.list(function (err, list) {
+								if (err) throw err;
+								console.log(list);
+								res.json({res: list})
+								c.end();
+							});
+						});
+					});
+				}
+			}
 			c.list(function (err, list) {
 				if (err) throw err;
 				console.log(list);
--- public/js/main.js
+++ public/js/main.js
@@ -7,14 +7,14 @@ function getInfo () {
 	var year = datePicked.getFullYear();
 	var month = datePicked.toLocaleString('en-us', {month: 'long'});
 	var day = datePicked.getDate();
-	var checked;
+	var brand;
 	for (var i = 0; i < checkboxes.length; i++) {
 		if (checkboxes[i].checked) {
-			checked = checkboxes[i].value;
+			brand = checkboxes[i].value;
 		}
 	}
 	$.ajax({
-		url: '/connect?brand='+checked,
+		url: '/connect?brand='+brand+'&year='+year+'&month='+month+'&day='+day,
 		method: 'GET',
 		success: function (res) {
 			console.log(res);
