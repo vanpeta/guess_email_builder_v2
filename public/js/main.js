7c1b62b86cec482c9f6d228079760c32af14d4f2
vanpeta <carlosallendesanz@gmail.com>
2017-03-02 15:58:13 -0800

    adding images to row works

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,10 @@
 var checkboxes = $('.brands');
 var calendar = $("#datepicker");
 var brand;
-
+var rowNum =1;
+var url = [];
+var alt = [];
+var href = [];
 //Generating Calendar
 calendar.datepicker();
 //Cleaning checkboxes when clicked on them
@@ -16,8 +19,58 @@ var numImages;
 $('#numImages').on('change', function() {
 	numImages = $(this).val();
 	for (var i = 0; i < numImages; i++) {
-		$('.rowSettings').append('<div><input type="file" id="image'+i+'" onchange="validator(this.files)"/></div>');
+		$('.rowSettings').append('<div><lable for="imageAlt'+i+'">Alt</lable><input type="text" class="imageAlt" id="imageAlt'+i+'" value="" /><lable for="imageHref'+i+'">href</lable><input type="text" class="imageHref" id="imageHref'+i+'" value="" /><input type="file" id="image'+i+'" onchange="validator(this)"/></div>');
 	}
+	addEventsAlt ();
+	addEventsHref ();
+});
+//HTML Row Code
+var contentRowAndTable = '<tr class="'+rowNum+'"><td class="content" align="left"><table width="100%" border="0" align="center" cellpadding="0" cellspacing="0"><tr>';
+var closeContentRowAndTable = '</tr></table></td></tr>';
+var row1images;
+var row2images;
+var row3images;
+var row4images;
+function addEventsAlt () {
+	return (
+		$('.imageAlt').on('change', function() {
+			var i = $(this).attr('id').slice(-1);
+			alt[i] = $(this).val();
+			addData (i);
+		})
+	)
+};
+function addEventsHref () {
+	return (
+		$('.imageHref').on('change', function() {
+			var i = $(this).attr('id').slice(-1);
+			href[i] = $(this).val();
+			addData (i);
+		})
+	)
+};
+
+function addData (i) {
+	switch (i) {
+		case '0':
+			return row1images = contentRowAndTable+'<td width="100%"><a href="'+href[0]+'"><img id="image0" alt="'+alt[0]+'" src="'+url[0]+'" width="582" style="display: block" /></a></td>'+closeContentRowAndTable;
+		break;
+		case '1':
+			return row2images = contentRowAndTable+'<td width="50%"><a href="'+href[0]+'"><img id="image0" alt="'+alt[0]+'" src="'+url[0]+'" width="291" style="display: block" /></a></td><td width="50%"><a href="'+href[1]+'"><img id="image1" alt="'+alt[1]+'" src="'+url[1]+'" width="291" style="display: block" /></a></td>'+closeContentRowAndTable;
+		break;
+		case '2':
+			return row3images = contentRowAndTable+'<td width="33.33%"><a href="'+href[0]+'"><img id="image0" alt="'+alt[0]+'" src="'+url[0]+'" width="194" style="display: block" /></a></td><td width="33.33%"><a href="'+href[1]+'"><img id="image1" alt="'+alt[1]+'" src="'+url[1]+'" width="194" style="display: block" /></a></td><td width="33.33%"><a href="'+href[2]+'"><img id="image2" alt="'+alt[2]+'" src="'+url[2]+'" width="194" style="display: block" /></a></td>'+closeContentRowAndTable;
+		break;
+		case '3':
+			return row4images = contentRowAndTable+'<td width="25%"><a href="'+href[0]+'"><img id="image0" alt="'+alt[0]+'" src="'+url[0]+'" width="145.5" style="display: block" /></a></td><td width="25%"><a href="'+href[1]+'"><img id="image1" alt="'+alt[1]+'" src="'+url[1]+'" width="145.5" style="display: block" /></a></td><td width="25%"><a href="'+href[2]+'"><img id="image2" alt="'+alt[2]+'" src="'+url[2]+'" width="145.5" style="display: block" /></a></td><td width="25%"><a href="'+href[3]+'"><img id="image3" alt="'+alt[3]+'" src="'+url[3]+'" width="145.5" style="display: block" /></a></td>'+closeContentRowAndTable;
+		break
+	}
+}
+$('#newRowButton').on('click', function () {
+	numImages = null;
+	$('#numImages').val('');
+	$('.rowSettings').empty();
+	rowNum++
 });
 //Making sure one brand is chosen and triggering send image to backend
 function validator (image) {
@@ -29,16 +82,17 @@ function validator (image) {
 	if (brand == undefined) {
 		return alert('Choose one brand');
 	}
-	sendData (image);
+	var id = $(image).attr('id');
+	sendData (image.files, id);
 };
 //Sending image and data to backend
-function sendData (image) {
+function sendData (image, id) {
 	convertTo64(image).then(function (imageData){
 		var datePicked = calendar.datepicker('getDate');
 		var year = datePicked.getFullYear();
 		var month = datePicked.toLocaleString('en-us', {month: 'long'});
 		var monthNumber = datePicked.getMonth()+1;
-		var imageName = $('#image').val().split('\\').pop().replace(/\s+/g, '_');
+		var imageName = $('#'+id).val().split('\\').pop().replace(/\s+/g, '_');
 		if (monthNumber < 10) {
 			monthNumber = '0'+monthNumber
 		}
@@ -59,7 +113,11 @@ function sendData (image) {
 				imageName: imageName
 			},
 			success: function (res) {
-				addImages (res.url);
+				console.log(res);
+				id = id.slice(-1);
+				url[id] = res.url;
+				addData(id)
+				addRow (id);
 			}
 		})
 	})
@@ -76,26 +134,23 @@ function convertTo64 (image) {
 		reader.readAsDataURL(image[0]);
 	})
 };
-//Generating preview and code to be donwloaded
-function addImages (url) {
-	var contentRowAndTable = '<tr><td class="content" align="left"><table width="100%" border="0" align="center" cellpadding="0" cellspacing="0"><tr>';
-	var closeContentRowAndTable = '</tr></table></td></tr>';
-	if (numImages == 1) {
-		$('.images').append(contentRowAndTable+'<td width="100%"><a href="#"><img alt="" src="'+url+'" width="582" style="display: block" /></a></td>'+closeContentRowAndTable);
-	}
-	else if (numImages == 2) {
-		$('.images').append(contentRowAndTable+'<td width="50%"><a href="#"><img alt="" src="'+url+'" width="291" style="display: block" /></a></td><td width="50%"><a href="#"><img alt="" src="'+url+'" width="291" style="display: block" /></a></td>'+closeContentRowAndTable);
-	}
-	else if (numImages == 3) {
-		$('.images').append(contentRowAndTable+'<td width="33.33%"><a href="#"><img alt="" src="'+url+'" width="194" style="display: block" /></a></td><td width="33.33%"><a href="#"><img alt="" src="'+url+'" width="194" style="display: block" /></a></td><td width="33.33%"><a href="#"><img alt="" src="'+url+'" width="194" style="display: block" /></a></td>'+closeContentRowAndTable);
-	}
-	else if (numImages == 4) {
-		$('.images').append(contentRowAndTable+'<td width="25%"><a href="#"><img alt="" src="'+url+'" width="145.5" style="display: block" /></a></td><td width="25%"><a href="#"><img alt="" src="'+url+'" width="145.5" style="display: block" /></a></td><td width="25%"><a href="#"><img alt="" src="'+url+'" width="145.5" style="display: block" /></a></td><td width="25%"><a href="#"><img alt="" src="'+url+'" width="145.5" style="display: block" /></a></td>'+closeContentRowAndTable);
-	}
-};
 
-
-
+function addRow (numRows) {
+	console.log (numRows);
+	switch (numRows) {
+		case '0':
+			$('.images').html(row1images);
+			break;
+		case '1':
+			$('.images').html(row2images);
+			break;
+		case '2':
+			$('.images').html(row3images);
+			break;
+		case '3':
+			$('.images').html(row4images);
+	}
+}
 
 
 
