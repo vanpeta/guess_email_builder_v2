44bb407c7f8db6a92b03e09d28bd1e649d16a986
vanpeta <carlosallendesanz@gmail.com>
2017-03-08 11:54:49 -0800

    selected brand works

--- public/js/controllers/emailController.js
+++ public/js/controllers/emailController.js
@@ -6,7 +6,29 @@
 
 		function emailController() {
 			var vm = this;
-			vm.test = "testing controller"
-		}
+			vm.showBrands = "";
+			vm.showMenu = showMenu;
+			vm.brand = "";
+			vm.selectBrand = selectBrand;
+			vm.selected = "";
 
+			function showMenu() {
+				if (vm.showBrands == "") {
+					vm.showBrands = "show";
+				}
+				else vm.showBrands = "";
+				return vm.showBrands 
+			}
+
+			function selectBrand(selectedBrand) {
+				vm.brand = selectedBrand.target.textContent;
+				for (var i = 0; i < document.getElementsByClassName('show').length; i++) {
+					document.getElementsByClassName('show')[i].id = "";
+				}
+
+				selectedBrand.target.id = "selected"
+			}
+
+
+		}
 })();
